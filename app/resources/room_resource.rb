11a1be3509caebf7d43ef243f4bd37516913a07f
Raghu Betina <raghu@firstdraft.com>
2020-10-21 18:27:16 +0000

    Generate graphiti resources for rooms

---
+++ app/resources/room_resource.rb
@@ -0,0 +1,12 @@
+class RoomResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :open_to_public, :boolean
+
+  # Direct associations
+
+  # Indirect associations
+
+end
