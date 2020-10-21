9f92c23e12c8c5275359bcf5b4ecb2c122688ab0
Raghu Betina <raghu@firstdraft.com>
2020-10-21 18:27:45 +0000

    Remove controller comments

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,26 +1,21 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: %i[show edit update destroy]
 
-  # GET /rooms
   def index
     @q = Room.ransack(params[:q])
     @rooms = @q.result(distinct: true).includes(:topics).page(params[:page]).per(10)
   end
 
-  # GET /rooms/1
   def show
     @topic = Topic.new
   end
 
-  # GET /rooms/new
   def new
     @room = Room.new
   end
 
-  # GET /rooms/1/edit
   def edit; end
 
-  # POST /rooms
   def create
     @room = Room.new(room_params)
 
@@ -31,7 +26,6 @@ class RoomsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /rooms/1
   def update
     if @room.update(room_params)
       redirect_to @room, notice: "Room was successfully updated."
@@ -40,7 +34,6 @@ class RoomsController < ApplicationController
     end
   end
 
-  # DELETE /rooms/1
   def destroy
     @room.destroy
     redirect_to rooms_url, notice: "Room was successfully destroyed."
@@ -48,12 +41,10 @@ class RoomsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_room
     @room = Room.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def room_params
     params.require(:room).permit(:name, :open_to_public)
   end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,26 +1,21 @@
 class TopicsController < ApplicationController
   before_action :set_topic, only: %i[show edit update destroy]
 
-  # GET /topics
   def index
     @q = Topic.ransack(params[:q])
     @topics = @q.result(distinct: true).includes(:room, :messages).page(params[:page]).per(10)
   end
 
-  # GET /topics/1
   def show
     @message = Message.new
   end
 
-  # GET /topics/new
   def new
     @topic = Topic.new
   end
 
-  # GET /topics/1/edit
   def edit; end
 
-  # POST /topics
   def create
     @topic = Topic.new(topic_params)
 
@@ -36,7 +31,6 @@ class TopicsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /topics/1
   def update
     if @topic.update(topic_params)
       redirect_to @topic, notice: "Topic was successfully updated."
@@ -45,7 +39,6 @@ class TopicsController < ApplicationController
     end
   end
 
-  # DELETE /topics/1
   def destroy
     @topic.destroy
     message = "Topic was successfully deleted."
@@ -58,12 +51,10 @@ class TopicsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_topic
     @topic = Topic.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def topic_params
     params.require(:topic).permit(:room_id, :name)
   end
