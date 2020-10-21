41cd81168992d02936dca714a5a975da2d89338f
Raghu Betina <raghu@firstdraft.com>
2020-10-21 18:26:52 +0000

    Whitelist columns in Room for ActiveAdmin

--- app/admin/rooms.rb
+++ app/admin/rooms.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Room do
 
+ permit_params :name, :open_to_public
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
