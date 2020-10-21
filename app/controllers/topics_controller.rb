cf9270d0dc63d8acda40faef75ebf662e665763c
Raghu Betina <raghu@firstdraft.com>
2020-10-21 18:27:00 +0000

    Display associated collections on show pages

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -42,8 +42,14 @@ class TopicsController < ApplicationController
   # DELETE /topics/1
   def destroy
     @topic.destroy
-    redirect_to topics_url, notice: 'Topic was successfully destroyed.'
+    message = "Topic was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to topics_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
